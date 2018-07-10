c9c8f39aec7314cae1c975b5d82ef750f807cc4e
NEGIhere <negigic@gmail.com>
2018-07-10 18:47:00 +0500

    Socket timeout changed to 5 secs Readme changed

--- src/com/paymon/wallet/Main.kt
+++ src/com/paymon/wallet/Main.kt
@@ -17,8 +17,6 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.system.exitProcess
 
-
-
 var authForm = CreateNewWallet()
 var walletForm = WalletForm()
 var loadForm = LoadExistWallet()
@@ -31,6 +29,7 @@ val ADDRESS_SIZE = HASH_SIZE + 1
 val ADDRESS_NULL = Address(ByteArray(ADDRESS_SIZE))
 val HASH_NULL = ByteArray(HASH_SIZE)
 val api = API()
+
 fun main(args: Array<String>) {
     try {
         api.account = WalletAccount(Hex.decode("8003FFFFF6882320DEEE46AC97069EC8C56928643907230EA3A80BA656312A5351768E1525CDDC40D661C4E6646FB5C0A9D52DB66A74C86F1ADD764AC3CF7BD66D16E9D619EF4090E996C350B75BA5CE856285775792700AF58203B65198012953514B5B0F1F86D8DDC8D6BB9ADF0A9665B6CADA1C6D166B93E704BDB063ACEAF6A519D6A58F88836E6B4AD431A576B13DBE59D4A603D833DAAD7EAF4AC5B48015522E1C3163A751EEAF34D8EE692806C88ABE6CB151DA79BE48C13CA894AC1DD3D4361B7F5574D1BC28754916B04849A066A8659CEEE9C334CEA0C327B99D458CC64257EC37C9B4216C9CE3469FD5B23DBC964488780E282790198443EA7A1F1FC824C51FDB7D18B5A6C188A2907446224B6C6FDCD264095E0BE053D293B544E22875470B55D58F5707EFD58E8DC5DDB475F25C5A660E63B202669524F02D4F973B5D4C2D52AC2C62BFCD5B54614F92F22B4B94E51E43AD0BEED8"))
@@ -38,14 +37,10 @@ fun main(args: Array<String>) {
         println(e.message)
         exitProcess(-1)
     }
-
-
-
     initListeners()
-
-    api.sendCoins(Address("PE138221B1A9CBEFCEAF03E17934A7373D6289F0536"), 100) {
-        println("Result $it")
-    }
+//    api.sendCoins(Address("PE138221B1A9CBEFCEAF03E17934A7373D6289F0536"), 100) {
+//        println("Result $it")
+//    }
 
     thread {
         updateThread()
@@ -155,33 +150,36 @@ fun updateAddress() {
 
 fun updateThread() {
     while (running) {
-        val addr = api.account?.address
-        if (addr != null) {
-            walletForm.setAddress(addr.toString())
-            val balance = api.getBalanceRequest(addr)
-            if(balance != null) {
-                walletForm.setBalance(balance.toInt())
-            }
-            println("Current balance: $balance")
-            val txHashes = api.getAddressTransactionHashes(addr)
-            if (txHashes != null) {
+        try {
+            val addr = api.account?.address
+            if (addr != null) {
+                walletForm.setAddress(addr.toString())
+                val balance = api.getBalanceRequest(addr)
+                if (balance != null) {
+                    walletForm.setBalance(balance.toInt())
+                }
+                println("Current balance: $balance")
+                val txHashes = api.getAddressTransactionHashes(addr)
+                if (txHashes != null) {
 //                for (hash in txHashes) {
 //                    println("request hash=${String(Hex.encode(hash))}")
 //                }
-                val txs = api.getTransactions(txHashes)
-                if(txs != null) {
-                    walletForm.list.clear()
-                    for (i in txs) {
-                        walletForm.addToList(i.hash.toString(),
-                                addr.toString(),
-                                i.address.toString(),
-                                i.value.toInt())
+                    val txs = api.getTransactions(txHashes)
+                    if (txs != null) {
+                        walletForm.list.clear()
+                        for (tx in txs) {
+                            walletForm.addToList(String(Hex.encode(tx.hash)),
+                                    addr.toString(),
+                                    tx.address.toString(),
+                                    tx.value.toInt())
+                        }
                     }
                 }
             }
-
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            walletForm.showExceptionMessage(true, e.message)
         }
-
         Thread.sleep(10000)
     }
 }
